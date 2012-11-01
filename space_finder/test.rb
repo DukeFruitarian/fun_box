e705b91319dff1727ab9808dd5ebe6f6330a70ca
DukeFruitarian <Duke1@sura.ru>
2012-11-01 13:07:02 +0400

    add some benchmark

--- space_finder/test.rb
+++ space_finder/test.rb
@@ -4,7 +4,7 @@ require File.join(File.dirname(__FILE__), "spacer/spacer")
 
 
 # количество элементов в массиве
-number_of_element = 1000
+number_of_element = 100000
 
 # максимальное количество пропущенных элементов
 number_of_spaces = 10
@@ -15,9 +15,25 @@ number_of_spaces.times do |num|
 end
 
 
-10000.times do
-  finder = SpaceFinder::Base.new(array,1)
+time_my = Benchmark.realtime do
+  100.times do
+    finder = SpaceFinder::Base.new(array,1)
+    finder.spaces
+  end
 end
 
-finder = SpaceFinder::Base.new(array,1)
-p finder.spaces
+time_standart = Benchmark.realtime do
+  100.times do
+    finder = []
+    array.each_with_index do |el,index|
+      if array[index+1] && array[index+1]-el > 1
+        (array[index+1]-el).times do |num|
+          finder << el+1+num
+        end
+      end
+    end
+  end
+end
+
+
+puts "time_my = #{time_my}", "time_standart = #{time_standart}"
